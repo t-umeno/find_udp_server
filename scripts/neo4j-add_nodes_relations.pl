c666809e24f12d599f31d12cbd37731097afe04f
Takashi Umeno <umeno.takashi@gmail.com>
2017-02-05 23:03:25 +0900

    add: import csv

--- scripts/neo4j-add_nodes_relations.pl
+++ scripts/neo4j-add_nodes_relations.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import getopt, sys
+import csv
 
 from neo4j.v1 import GraphDatabase, basic_auth
 
@@ -8,12 +9,14 @@ def usage():
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+        opts, args = getopt.getopt(sys.argv[1:], "hi:v", ["help", "input="])
     except getopt.GetoptError as err:
         # print help information and exit:
         usage()
         sys.exit(2)
     output = None
+    input_super_mediator_file = None
+    input_super_mediator = False
     verbose = False
     for o, a in opts:
         if o == "-v":
@@ -21,8 +24,9 @@ def main():
         elif o in ("-h", "--help"):
             usage()
             sys.exit
-        elif o in ("-o", "--output"):
-            output = a
+        elif o in ("-i", "--input"):
+            input_super_mediator_file = a
+            input_super_mediator = True
         else:
             assert False, "unhandled option"
 
@@ -30,6 +34,13 @@ def main():
 
     driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", password))
     session = driver.session()
+
+    if input_super_mediator == True:
+        f = open(input_super_mediator_file, 'r')
+        reader = csv.reader(f, delimiter='|')
+        for row in reader:
+            print row
+        f.close()
 
     session.close()
 
