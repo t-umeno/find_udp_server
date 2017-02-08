ab5f00dc34389e70931a6b7f0d72fa37cd230d07
Takashi Umeno <umeno.takashi@gmail.com>
2017-02-08 23:39:07 +0900

    add: result = session.run("MATCH(a:udp_node{IP:\"%s\",port:%d}, (b:udp_node{IP:\"%s\",port:%d} MERGE (a-c:comm->b)" % (row[0],int(row[1]),row[2],int(row[3])))

--- scripts/neo4j-add_nodes_relations.py
+++ scripts/neo4j-add_nodes_relations.py
@@ -97,6 +97,8 @@ def main():
                     
         for k,v in udp_comm.items():
             print k,v
+            row = k.split(',')
+            result = session.run("MATCH(a:udp_node{IP:\"%s\",port:%d}, (b:udp_node{IP:\"%s\",port:%d} MERGE (a-c:comm->b)" % (row[0],int(row[1]),row[2],int(row[3])))
 
     session.close()
 
